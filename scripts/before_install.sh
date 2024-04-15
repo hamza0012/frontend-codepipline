c741b1ce223d7cf80b23d2b02ac0b2aae8dba81a
hamza0012 <muhammadhamza.12.a@gmail.com>
2024-04-15 12:07:31 +0500

    OK

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -9,7 +9,7 @@ nvm install node
 
 sudo yum install npm -y
 sudo yum install nginx -y
-sudo amazon-linux-extras install epel
-yum install --enablerepo="epel" ufw
-sudo ufw enable
-sudo ufw allow 'Nginx HTTP'
+# sudo amazon-linux-extras install epel
+# yum install --enablerepo="epel" ufw
+# sudo ufw enable
+# sudo ufw allow 'Nginx HTTP'
