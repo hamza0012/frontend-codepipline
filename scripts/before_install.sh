79a06fd63563e3d05d0329c9c5c4bf4c0bb53458
hamza0012 <muhammadhamza.12.a@gmail.com>
2024-04-15 11:55:01 +0500

    change before run script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # navigate to app folder
-cd /app
 
 # install node and npm
-apt-get install curl
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-apt-get install nodejs -y
-apt-get install npm -y
-apt install nginx -y
+sudo yum install curl
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash . ~/.nvm/nvm.sh
+nvm install node
+
+sudo yum install npm -y
+sudo yum install nginx -y
 ufw allow 'Nginx HTTP'
